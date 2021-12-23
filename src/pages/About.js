107c590bed4704f1201fa1339e70e06604a827dc
Guilherme Garcia <guilherme4garcia@gmail.com>
2021-12-23 15:23:20 -0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus repellat
+        itaque nihil dolore distinctio laudantium fugiat, unde eos expedita id
+        obcaecati ex pariatur exercitationem quam quas aperiam eaque velit ad
+        necessitatibus reprehenderit odio sed hic voluptate. Velit natus
+        reiciendis accusamus.
+      </p>
+    </section>
+  )
+}
+
+export default About
